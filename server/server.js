0eadcf74036df68a5a42dbdfddb7ca2efeb6a68d
Rubul Islam <workwithrubul23@gmail.com>
2026-07-26 15:12:40 +0530

    Update Indilens Server

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "https://indilens-website-jyij.vercel.app/",
       "https://indilens-website.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
